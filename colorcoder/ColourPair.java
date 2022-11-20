10fde61a656e87853e4b8f18ec09908a72cc514c
varshinichaithra <78721485+varshinichaithra@users.noreply.github.com>
2022-11-20 16:10:03 +0530

    Add files via upload

---
+++ colorcoder/ColourPair.java
@@ -0,0 +1,45 @@
+package colorcoder;
+
+public class ColorPair {
+        final static String MajorColorNames[] = {
+	        "White", "Red", "Black", "Yellow", "Violet"
+	    };
+	    final static int numberOfMajorColors = MajorColorNames.length;
+	    final static String MinorColorNames[] = {
+	        "Blue", "Orange", "Green", "Brown", "Slate"
+	    };
+	    final static int numberOfMinorColors = MinorColorNames.length;
+        
+        private MajorColor majorColor;
+        private MinorColor minorColor;
+        
+        public ColorPair(MajorColor major, MinorColor minor)
+        {
+            majorColor = major;
+            minorColor = minor;
+        }
+        public MajorColor getMajor() {
+            return majorColor;
+        }
+        public MinorColor getMinor() {
+            return minorColor;
+        }
+        String ToString() {
+            String colorPairStr = MajorColorNames[majorColor.getIndex()];
+            colorPairStr += " ";
+            colorPairStr += MinorColorNames[minorColor.getIndex()];
+            return colorPairStr;
+        }
+    };
+
+    static ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            MajorColor.fromIndex(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            MinorColor.fromIndex(zeroBasedPairNumber % numberOfMinorColors);
+        return new ColorPair(majorColor, minorColor);
+    }
+    static int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major.getIndex() * numberOfMinorColors + minor.getIndex() + 1;
+    }
